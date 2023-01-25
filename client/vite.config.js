3c9494728f02863d65473115da2bf2a45286149d
Jeremy Cinq-Mars <jeremy@cinqmars.ca>
2023-01-25 18:00:48 -0500

    ViteConfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
     global: "globalThis",
     "process.env": {},
   },
+  build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
+
